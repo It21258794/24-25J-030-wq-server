4c12aa2a9cfdb9cf0d6ca84cf134b7bca98b32b6
It21258794 <it21258794@my.sliit.lk>
2024-12-18 18:30:14 +0530

    remove scan package

--- app/main-application/src/main/java/com/waterboard/waterqualityprediction/MainApplication.java
+++ app/main-application/src/main/java/com/waterboard/waterqualityprediction/MainApplication.java
@@ -3,7 +3,7 @@ package com.waterboard.waterqualityprediction;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.waterboard.waterqualityprediction")
+@SpringBootApplication()
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
