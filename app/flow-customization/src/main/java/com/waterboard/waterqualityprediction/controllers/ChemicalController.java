32b245c5f927a54550b8ddd5cf96f0e7443a1965
IT21174308 <it21174308@my.sliit.lk>
2025-01-19 10:27:49 +0530

    feat(fetch all chemicals):create functionality to fetch all chemicals

--- app/flow-customization/src/main/java/com/waterboard/waterqualityprediction/controllers/ChemicalController.java
+++ app/flow-customization/src/main/java/com/waterboard/waterqualityprediction/controllers/ChemicalController.java
@@ -2,9 +2,12 @@ package com.waterboard.waterqualityprediction.controllers;
 
 import com.waterboard.waterqualityprediction.dto.ChemicalDTO;
 import com.waterboard.waterqualityprediction.models.Chemical;
+import com.waterboard.waterqualityprediction.models.Step;
 import com.waterboard.waterqualityprediction.services.ChemicalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/chemicals")
@@ -13,9 +16,18 @@ public class ChemicalController {
     @Autowired
     private ChemicalService chemicalService;
 
+    //function to add new chemical
     @PostMapping("/create")
     public Chemical addChemical(@RequestBody ChemicalDTO chemicalDTO) {
         return chemicalService.addChemical(chemicalDTO);
     }
+
+    //function to fetch all chemicals
+
+    @GetMapping ("/get/all-chemicals")
+    public List<Chemical> getAllSteps() {
+        return chemicalService.getAllChemicals();
+    }
+
 
 }
--- app/flow-customization/src/main/java/com/waterboard/waterqualityprediction/services/ChemicalService.java
+++ app/flow-customization/src/main/java/com/waterboard/waterqualityprediction/services/ChemicalService.java
@@ -2,10 +2,14 @@ package com.waterboard.waterqualityprediction.services;
 
 import com.waterboard.waterqualityprediction.dto.ChemicalDTO;
 import com.waterboard.waterqualityprediction.models.Chemical;
+import com.waterboard.waterqualityprediction.models.Step;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ChemicalService {
@@ -13,6 +17,7 @@ public class ChemicalService {
     @PersistenceContext
     private EntityManager entityManager;
 
+    //function to add new chemical
     @Transactional
     public Chemical addChemical(ChemicalDTO chemicalDTO) {
         Chemical chemical = new Chemical();
@@ -22,4 +27,17 @@ public class ChemicalService {
         entityManager.persist(chemical); // Persisting the chemical manually
         return chemical;
     }
+
+    public List<Chemical> getAllChemicals() {
+        try {
+            // Ensure that you use the correct JPQL query
+            TypedQuery<Chemical> query = entityManager.createQuery("SELECT c FROM Chemical c", Chemical.class);
+            List<Chemical> chemicals = query.getResultList();
+            return chemicals;
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
 }
